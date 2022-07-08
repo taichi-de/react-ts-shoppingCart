87920f6150a14f430267a1bd558bcdcdca549d30
taichi-de <61309248+taichi-de@users.noreply.github.com>
2022-07-08 09:36:56 +0200

    finish StorePage logic

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import {Row, Col} from "react-bootstrap"
+import { StoreItem } from '../components/StoreItem'
+import storeItems from "../data/items.json"
 
 export default function  () {
   return (
-    <div> </div>
+    <>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
